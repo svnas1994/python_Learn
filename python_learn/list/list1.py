6f9d5bb13fce0cbc292c2c8ca73125495686da6b
svnas1994 <sriramsairam347@gmail.com>
2021-05-14 01:53:37 +0530

    adding comments to existing code
    Change-Id: Id0946147e3d319e6719d5d801999ac9b5ce88409

--- python_learn/list/list1.py
+++ python_learn/list/list1.py
@@ -1,7 +1,10 @@
 """this project is for list exercise"""
 a = ["sairam","svn",23, 34.2, 6+3j, ("s","a",3, 2, 1), {"name":"lenovo", "chip":"intel"}]
+''' Printing the first element '''
 print (a[0])
+'''printing the first 2 elements '''
 print (a[1:3])
+''' exercising the for loop on a list '''
 for i in range (len(a)):
     print (i, a[i])
     
