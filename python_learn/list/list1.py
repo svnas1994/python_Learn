a3c90a708ee8db58c3ddf993726d0b15dde62469
svnas1994 <sriramsairam347@gmail.com>
2021-05-14 01:51:55 +0530

    adding the list exercise

---
+++ python_learn/list/list1.py
@@ -0,0 +1,7 @@
+"""this project is for list exercise"""
+a = ["sairam","svn",23, 34.2, 6+3j, ("s","a",3, 2, 1), {"name":"lenovo", "chip":"intel"}]
+print (a[0])
+print (a[1:3])
+for i in range (len(a)):
+    print (i, a[i])
+    
