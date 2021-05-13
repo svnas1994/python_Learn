e33a65e71afb46757e48afbc81c7023fe51de9f9
svnas1994 <sriramsairam347@gmail.com>
2021-05-14 01:24:16 +0530

    arithmatic operation functions file

---
+++ python_learn/arithmatic_operation_exercise.py
@@ -0,0 +1,30 @@
+"""This programme is for arithmatic operation of two numbers"""
+a = int(input("enter the number"))
+b = int(input("enter the number"))
+print ("for addtion enter add\
+        for subtraction etner sub\
+        for multipliction enter mul\
+        for remainder of division add rem")
+c=input("enter your required operation").lower()
+def add(a, b):
+    '''adding the two numbers'''
+    print (a+b)
+def sub(a, b):
+    '''subtracting the two numbers'''
+    print (a-b)
+def mul(a, b):
+    '''multiplication the two numbers'''
+    print (a-b)
+def rem(a,b):
+    ''' remainder of divison'''
+    print(a%b)
+if c == "add":
+    add(a, b)
+elif c == "sub":
+    sub(a, b)
+elif c == "mul":
+    mul(a, b)
+elif c == "rem":
+    rem(a,b)
+else:
+    print ("the requested operation is not supported")
